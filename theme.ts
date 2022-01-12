497310640a7c3bde56184b37c8c034ced08ac3b3
Stanislav Shalovilo <shalovilo@gmail.com>
2022-01-12 18:29:12 +0300

    feat/add initial page

---
+++ theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+const defaultTheme: DefaultTheme = {
+  colors: {
+    main: "#050505",
+    primary: "#1b9aaa",
+    secondary: "#DDDBCB",
+    cream: "#F5F1E3",
+    white: "#fff",
+  },
+};
+
+export default defaultTheme;
